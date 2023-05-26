177bafa801d155da3a0eee7ea9840dde9df6e03d
salim <02200163.cst@rub.edu.bt>
2023-05-26 07:09:42 +0600

    linter

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  eslint:{
+    ignoreDuringBuilds:true
+  },
   //image
   images: {
     domains: ["picsum.photos", "images.unsplash.com","uploadthing.com","lh3.googleusercontent.com"],
